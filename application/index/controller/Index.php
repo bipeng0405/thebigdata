c01fb7b5de60ebce4216099c20434b547ed2fdad
毕鹏 <bipeng0405@163.com>
2016-06-05 15:45:09 +0800

    修改bug

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -4,7 +4,7 @@ namespace app\index\controller;
 use think\Controller;
 class Index extends Controller
 {
-    public function hello($name = bipeng)
+    public function hello($name = 'bipeng')
     {
         $this->assign('name',$name);
         return $this->fetch();
