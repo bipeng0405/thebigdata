74b4462651673a81ce2d9f6da6038faf6137bc9d
毕鹏 <bipeng0405@163.com>
2016-06-05 14:55:09 +0800

    测试tp5

--- application/database.php
+++ application/database.php
@@ -16,13 +16,13 @@ return [
     // 数据库连接DSN配置
     'dsn'         => '',
     // 服务器地址
-    'hostname'    => '127.0.0.1',
+    'hostname'    => 'mysql.hostinger.com.hk',
     // 数据库名
-    'database'    => '',
+    'database'    => 'u980621105_data',
     // 数据库用户名
-    'username'    => 'root',
+    'username'    => 'u980621105_data',
     // 数据库密码
-    'password'    => '',
+    'password'    => 'dFtYcp2JB0',
     // 数据库连接端口
     'hostport'    => '',
     // 数据库连接参数
