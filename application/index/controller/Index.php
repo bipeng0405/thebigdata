7ee882edac5fda3b182b233454762e2646ed382b
毕鹏 <bipeng0405@163.com>
2016-06-06 22:43:00 +0800

    fixbugs

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,21 +1,32 @@
 <?php
 namespace app\index\controller;
+use think\Db;
+use think\Controller;
+class Index extends Controller
+{
+    public function index()
+    {
+        $data = Db::name('companys')->find();
+        $this->assign('result',$data);
+        return $this->fetch();
+    }
+}
+
 
 class Blog
 {
-
     public function get($id)
     {
-        return '查看id=' . $id . '的内容';
+        return '查看id= '.$id.' 的内容';
     }
 
     public function read($name)
     {
-        return '查看name=' . $name . '的内容';
+        return '查看name= ' .$name. '的内容';
     }
 
-    public function archive($year, $month)
+    public function archive($year,$month)
     {
-        return '查看' . $year . '/' . $month . '的归档内容';
+        return '查看' .$year. '/'.$month. '存档';
     }
 }
